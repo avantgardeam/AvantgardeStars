7087ea3c5251a18795ba45309e1677442675c19b
avantgardeam <avantgardeam@gmail.com>
2023-08-22 01:39:19 -0300

    add support to upload table to aws s3. plus, add quality as a factor.

--- functions.R
+++ functions.R
@@ -1,39 +1,31 @@
+##################################################################
+##                      Upload Table to S3                      ##
+##################################################################
 
-#################################################################
-##                     Get Number of Stars                     ##
-#################################################################
-
-get_number_of_stars <- function(factor_metric){
-  #' Assign Stars.
-  #' @description This function assigns a star rating (ranging from 1 to 5) 
-  #' to each asset based on how well it ranks in comparison to 
-  #' other assets with respect to a given factor. 
-  #' 
-  #' The higher the 
-  #' asset's rank, the higher the star rating it will receive. 
-  #' Specifically, the top 10% of assets will receive a 5-star 
-  #' rating, the next 20% (i.e., assets ranked between 10% and 30%) 
-  #' will receive a 4-star rating, the next 40% 
-  #' (i.e., assets ranked between 30% and 70%) will receive a 3-star 
-  #' rating, the next 20% (i.e., assets ranked between 70% and 90%) 
-  #' will receive a 2-star rating, and the bottom 10% of assets will 
-  #' receive a 1-star rating. 
-  #' @param factor_metric numeric vector. Vector containing the values for a factor metric (PE RATIO, e.g.).
-  #' @returns Categorical vector indicating the star rating of each asset.
-  
-  ranked_factor_metric <- rank(factor_metric)
-  
-  len <- length(ranked_factor_metric)
-  
-  # Defines breaks for each star level based on the length of the ranked vector
-  breaks <- round(c(
-    1, len * 0.1, len * 0.3, len * 0.7, len * 0.9, len
-  ))
-  
-  # Break points are used to determine which star level each asset will receive 
-  number_of_stars <- cut(
-    ranked_factor_metric, breaks = breaks, include.lowest = TRUE, labels = 1:5
+upload_df_to_aws_s3 <- function(df){
+  svc <- s3(
+    config = list(
+      credentials = list(
+        creds = list(
+          access_key_id = keyring::key_get("aws_keys", "AWS_ACCESS_KEY_ID"),
+          secret_access_key = keyring::key_get("aws_keys", "AWS_SECRET_ACCESS_KEY")
+        )
+      ),
+      region = keyring::key_get("aws_keys", "AWS_REGION")
+    )
   )
   
-  return(number_of_stars)
+  write_csv(df, "current_stars_ranking.csv")
+  
+  svc$put_object(
+    Body = "current_stars_ranking.csv",
+    Bucket = "avantgarde-stars",
+    Key = "current_stars_ranking.csv",
+    Metadata = list(
+      report_reference_date = Sys.Date()
+    )
+  )
+  
+  file.remove("current_stars_ranking.csv")
 }
+
